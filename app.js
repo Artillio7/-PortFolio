791bce55e11940b7a045658a2eeb81aef5ae289f
Artillio7 <90572974+Artillio7@users.noreply.github.com>
2023-09-14 15:19:09 +0200

    Mise à jour du portfolio
    Ajouts d'animations, optimisation de la responsivité et intégration de nouveaux composants Bootstrap.

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const allNavItems = document.querySelectorAll('.nav-item');
-const navbar = document.querySelector('.navbar-collapse');
 
-
-allNavItems.forEach(item => item.addEventListener('click', () => {
-
+const navbarToggler = document.querySelector('.navbar-toggler');
+navbarToggler.addEventListener('click', function() {
     navbar.classList.toggle('show');
+});
 
-}));
+const descriptionButtons = document.querySelectorAll('[data-toggle="modal"]');
+descriptionButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        const description = this.closest('.card-body').querySelector('.collapse').textContent;
+        document.querySelector('.modal-body').textContent = description;
+    });
+});
+
+ 

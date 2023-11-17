78a647041de9ba1458423407e88a2e1c318914c0
Artillio7 <jtchotcheuyepmo@gmail.com>
2023-11-17 20:37:13 +0100

    editNavbar
    hamburgerB

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const navbarToggler = document.querySelector('.navbar-toggler');
-navbarToggler.addEventListener('click', function() {
-    navbar.classList.toggle('show');
-    navbarToggler.classList.toggle('active');
-});
+const toggler = document.querySelector(".hamburger");
+const navLinksContainer = document.querySelector(".navlinks-container");
 
+const toggleNav = e => {
+  // Animation du bouton
+  toggler.classList.toggle("open");
+
+  const ariaToggle =
+    toggler.getAttribute("aria-expanded") === "true" ? "false" : "true";
+  toggler.setAttribute("aria-expanded", ariaToggle);
+
+  // Slide de la navigation
+  navLinksContainer.classList.toggle("open");
+};
+
+toggler.addEventListener("click", toggleNav);
+
+
+new ResizeObserver(entries => {
+  if (entries[0].contentRect.width <= 900){
+    navLinksContainer.style.transition = "transform 0.4s ease-out";
+  } else {
+    navLinksContainer.style.transition = "none";
+  }
+}).observe(document.body)
+ 
 const descriptionButtons = document.querySelectorAll('[data-toggle="modal"]');
 descriptionButtons.forEach(button => {
     button.addEventListener('click', function() {
@@ -12,23 +32,5 @@ descriptionButtons.forEach(button => {
     });
 });
 
- 
 
 AOS.init();
-
-document.querySelector('.navbar-toggler').addEventListener('click', function() {
-    var icon = this.querySelector('.navbar-toggler-icon');
-    if (icon.classList.contains('active')) {
-        icon.classList.remove('active');
-    } else {
-        icon.classList.add('active');
-    }
-});
-
-document.addEventListener('click', function(event) {
-  var isClickInside = document.querySelector('.navbar').contains(event.target);
-  var isOpened = document.querySelector('.navbar-collapse').classList.contains('show');
-  if (!isClickInside && isOpened) {
-    document.querySelector('.navbar-toggler').click();
-  }
-});
